a592d6d5414526e67690af66466fb6e0b4f76bb8
Jonathan <djonathan88@hotmail.com>
2021-04-04 23:39:15 +0200

    practice 4-1 Creating product mangemement app

---
+++ java-se-11-prog-complete/labs/practices/ProductManagement/src/labs/pm/app/Shop.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package labs.pm.app;
+
+import java.math.BigDecimal;
+import labs.pm.data.Product;
+
+/**
+ *
+ * @author Jonathan
+ */
+public class Shop {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Product p1 = new Product();
+        p1.setId(101);
+        p1.setName("Tea");
+        p1.setPrice(BigDecimal.valueOf(1.99));
+
+        System.out.println(p1.getId() + ","+p1.getName()+","+p1.getPrice());
+    }
+
+}
