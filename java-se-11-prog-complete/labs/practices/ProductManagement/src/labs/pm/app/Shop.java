f0a8f3bb9a125e4dba2a7d741eb04588100f3d90
Jonathan <djonathan88@hotmail.com>
2021-04-05 17:16:14 +0200

    practice 6-1 Extending classes

--- java-se-11-prog-complete/labs/practices/ProductManagement/src/labs/pm/app/Shop.java
+++ java-se-11-prog-complete/labs/practices/ProductManagement/src/labs/pm/app/Shop.java
@@ -17,6 +17,9 @@
 package labs.pm.app;
 
 import java.math.BigDecimal;
+import java.time.LocalDate;
+import labs.pm.data.Drink;
+import labs.pm.data.Food;
 import labs.pm.data.Product;
 import static labs.pm.data.Rating.*;
 
@@ -34,16 +37,16 @@ public class Shop {
     public static void main(String[] args) {
 
         Product p1 = new Product(101, "Tea", BigDecimal.valueOf(1.99));
-        Product p2 = new Product(102, "Coffee", BigDecimal.valueOf(1.99), FOUR_STAR);
-        Product p3 = new Product(103, "Cake", BigDecimal.valueOf(3.99), FIVE_STAR);
+        Product p2 = new Drink(102, "Coffee", BigDecimal.valueOf(1.99), FOUR_STAR);
+        Product p3 = new Food(103, "Cake", BigDecimal.valueOf(3.99), FIVE_STAR, LocalDate.now().plusDays(2));
         Product p4 = new Product();
         Product p5 = p3.applyRating(TWO_STAR);
-        
-        System.out.println(p1.getId() + "," + p1.getName() + "," + p1.getPrice() + "," + p1.getDiscount() +","+ p1.getRating().getStars());
-        System.out.println(p2.getId() + "," + p2.getName() + "," + p2.getPrice() + "," + p2.getDiscount() +","+ p2.getRating().getStars());
-        System.out.println(p3.getId() + "," + p3.getName() + "," + p3.getPrice() + "," + p3.getDiscount() +","+ p3.getRating().getStars());
-        System.out.println(p4.getId() + "," + p4.getName() + "," + p4.getPrice() + "," + p4.getDiscount() +","+ p4.getRating().getStars());
-        System.out.println(p5.getId() + "," + p5.getName() + "," + p5.getPrice() + "," + p5.getDiscount() +","+ p5.getRating().getStars());
+
+        System.out.println(p1.getId() + "," + p1.getName() + "," + p1.getPrice() + "," + p1.getDiscount() + "," + p1.getRating().getStars());
+        System.out.println(p2.getId() + "," + p2.getName() + "," + p2.getPrice() + "," + p2.getDiscount() + "," + p2.getRating().getStars());
+        System.out.println(p3.getId() + "," + p3.getName() + "," + p3.getPrice() + "," + p3.getDiscount() + "," + p3.getRating().getStars());
+        System.out.println(p4.getId() + "," + p4.getName() + "," + p4.getPrice() + "," + p4.getDiscount() + "," + p4.getRating().getStars());
+        System.out.println(p5.getId() + "," + p5.getName() + "," + p5.getPrice() + "," + p5.getDiscount() + "," + p5.getRating().getStars());
     }
 
 }
