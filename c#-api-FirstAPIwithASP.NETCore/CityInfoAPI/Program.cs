c3032d885c653ca75441916fbb93c23006b1850d
jonadv <djonathan88@hotmail.com>
2021-10-22 17:04:28 +0200

    5.4 - Demo: Logging to a File
    https://www.nuget.org/packages/NLog.Web.AspNetCore/
    
    Install-Package NLog.Web.AspNetCore -Version 4.14.0

--- c#-api-FirstAPIwithASP.NETCore/CityInfoAPI/Program.cs
+++ c#-api-FirstAPIwithASP.NETCore/CityInfoAPI/Program.cs
@@ -1,6 +1,8 @@
+using System;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore;
 using Microsoft.Extensions.Hosting;
+using NLog.Web;
 
 namespace CityInfoAPI
 {
@@ -9,12 +11,27 @@ namespace CityInfoAPI
         //asp.net 3.0
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var logger = NLogBuilder
+                .ConfigureNLog("nlog.config")
+                .GetCurrentClassLogger();
+            try {
+
+                logger.Info("Initializing application...");
+                CreateHostBuilder(args).Build().Run();
+            }
+            catch (Exception ex) {
+                logger.Error(ex, "Application stopped because of exception.");
+                throw;
+            }
+            finally {
+                NLog.LogManager.Shutdown();
+            }
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder => { webBuilder.UseStartup<Startup>(); });
+                .ConfigureWebHostDefaults(webBuilder => { webBuilder.UseStartup<Startup>(); })
+                .UseNLog();
     }
     //asp.net 2.x
 /*        public static void Main(string[] args)
